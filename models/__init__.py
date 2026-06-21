e4f803116e4ec85f71b07de017439da1327d70ac
jossmarsala <marsalahjosefina@gmail.com>
2026-06-20 21:23:00 -0300

    feat: add ProductoUpdate model to product definitions and exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,12 +1,12 @@
 from models.venta import VentaCreate, VentaResponse, VentaDetalleCompleto, DetalleVentaResponse, ItemVenta
-from models.producto import ProductoCreate, ProductoResponse, ProductoConStock, PrecioUpdate
+from models.producto import ProductoCreate, ProductoResponse, ProductoConStock, PrecioUpdate, ProductoUpdate
 from models.cliente import ClienteCreate, ClienteResponse, MovimientoCCResponse
 from models.usuario import UsuarioResponse
 from models.alerta import AlertaResponse
 
 __all__ = [
     "VentaCreate", "VentaResponse", "VentaDetalleCompleto", "DetalleVentaResponse", "ItemVenta",
-    "ProductoCreate", "ProductoResponse", "ProductoConStock", "PrecioUpdate",
+    "ProductoCreate", "ProductoResponse", "ProductoConStock", "PrecioUpdate", "ProductoUpdate",
     "ClienteCreate", "ClienteResponse", "MovimientoCCResponse",
     "UsuarioResponse",
     "AlertaResponse",
